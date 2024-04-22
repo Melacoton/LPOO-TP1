7d21bc11136eea00f63b72df8874e657bfd7ca08
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-21 21:29:15 -0300

    Update 3.cpp
    ola

--- 3.cpp
+++ 3.cpp
@@ -1,3 +1,6 @@
+/*3) Se solicita realizar un software que pida al usuario los datos de los catetos de un triángulo
+rectángulo y luego me informe cual es el valor de su hipotenusa*/
+
 #include <stdio.h>
 #include <math.h>
 int cateto1, cateto2, hipotenusa;
@@ -8,7 +11,7 @@ main(){
 	printf("Ingrese cateto 2: ");
 	scanf("%d", & cateto2);
 	
-	hipotenusa = sqrt(cateto1+cateto1 + cateto2*cateto2);
+	hipotenusa = sqrt(cateto1*cateto1 + cateto2*cateto2);
 	
 	printf("\nEl valor de su hipotenusa es %d", hipotenusa);
 }
