ac35abb41cded113ce931e35c6212bc259933cc1
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-21 21:27:50 -0300

    Update 1.cpp
    ola

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,8 @@
+/*1) Se debe realizar un programa el cual se solicite la siguiente información al usuario
+Nombre, Apellido, Fecha de nacimiento, y DNI. Y luego informe si puede o no votar y los
+datos de Apellido, Nombre y DNI
+Sabiendo que las elecciones son el 30 de septiembre, y pueden votar los mayores de 16 años*/
+
 #include <stdio.h>
 char nombre [150], apellido [150];
 int dni, dia, anio, mes, anio_v=2024;
