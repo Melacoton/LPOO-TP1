15c397cbd1b22a5e04e8f17aec054400a0752dfd
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-21 21:54:42 -0300

    Update 7.cpp
    ola

--- 7.cpp
+++ 7.cpp
@@ -1,28 +1,30 @@
-
+/*7) Dada una lista de números informar
+a) la cantidad de números ingresada
+b) la cantidad de números que son pares
+c) la cantidad de números que son impares*/
 #include <stdio.h>
-int N, i, cont=0, cont2=0, par=0, impar=0;
-int main(){
-	printf("Ingrese la cantidad total de numeros: ");
-	scanf("%d", & N);
-	int lista[N];
-	for(i=0;i<N;i++){
-		printf("Ingrese el numero: ");
+int i, n, cont00, par=0, impar=0; 
+main(){
+	printf("Ingrese la cantidad de valores: ");
+	scanf("%d", & n);
+	
+	int lista[n];
+	
+	for(i=0;i<n;i++){
+		printf("\nIngrese el valor %d:", i+1);
 		scanf("%d", & lista[i]);
-		cont++;
 	}
 	
-	for(i=0;i<N;i++){
-		printf("%d ", lista[i]);
+	for(i=0;i<n;i++){
+		if(lista[i]%2 ==0){
+			par++;
+		}
+		else{
+			impar++;
+		}
 	}
-	for(i=0;i<N;i++){
-		if(lista[i]%2==0){
-		par++;
-	}
-	    else{
-		impar++;
-	}
-	}
-	printf("\nLa cantidad de numeros ingresada es %d", cont);
-	printf("\nLa cantidad de numeros pares es %d", par);
-	printf("\nLa cantidad de numeros impares es %d", impar);
+	printf("\nLa cantidad de numeros ingresada es %d", n);
+	printf("\nLa cantidad de numeros par ingresada es %d", par);
+	printf("\nLa cantidad de numeros impar ingresada es %d", impar);
+
 }
