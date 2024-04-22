5a38508f0564dca622f76bf43ccc4111b67b96ac
Melacoton <145458286+Melacoton@users.noreply.github.com>
2024-04-21 21:28:30 -0300

    Update 2.cpp
    ola

--- 2.cpp
+++ 2.cpp
@@ -1,3 +1,10 @@
+/*2) Realizar una mejora al programa anterior con las siguientes condiciones
+Sabiendo que las elecciones son el 30 de septiembre,
+Menor de 16 años no puede votar
+Mayor de 16 años (inclusive) y menor de 18 años puede votar
+Mayor de 18 años (inclusive) y menor de 80 años debe votar
+Mayor de 80 años (inclusive) puede votar*/
+
 #include <stdio.h>
 char nombre [150], apellido [150];
 int dni, dia, anio, mes, anio_v=2024, dia_v=30, mes_v=9;
@@ -15,7 +22,6 @@ int main(){
 	scanf("%d", & dni);
 	
 	int edad = anio_v-anio;
-	
 	
 	if(edad<16){
 		printf("Nombre:%s, Apellido:%s, Edad: %d DNI:%d", nombre, apellido, edad, dni);
